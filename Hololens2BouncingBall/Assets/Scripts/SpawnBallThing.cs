c8e493d83b68642305f6c7790d49f1db1fa1db06
nakisa <nakisa.donnelly@gmail.com>
2020-08-20 19:08:25 -0600

    Added Unity project

---
+++ Hololens2BouncingBall/Assets/Scripts/SpawnBallThing.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBallThing : MonoBehaviour
+{
+    public GameObject ballPrefab;
+
+    public void SpawnBall()
+    {
+        Vector3 position = new Vector3(transform.position.x, transform.position.y + 0.2f, transform.position.z);
+        GameObject spawnedBall = Instantiate(ballPrefab, position, transform.rotation);
+    }
+}
